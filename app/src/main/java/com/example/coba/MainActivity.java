b5b27e4beaf0281617cbb5fc1a07610d27058b4f
titikkhoirunn <titikkhoirunn@gmail.com>
2019-12-26 14:46:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/coba/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.coba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button barvolume,moveactivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        barvolume =findViewById(R.id.btn_barvolume);
+        moveactivity= findViewById(R.id.btn_move_activity);
+        Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+
+        barvolume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent barvolume = new Intent(getApplicationContext(), BarVolume.class);
+                startActivity(barvolume);
+            }
+        });
+
+        moveactivity.setOnClickListener(this);
+        btnMoveWithDataActivity.setOnClickListener(this);
+        btnDialPhone.setOnClickListener(this);
+        }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btn_move_activity:
+                    Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                    startActivity(moveIntent);
+                    break;
+                case R.id.btn_move_activity_data:
+                    Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                    moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
+                    moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
+                    startActivity(moveWithDataIntent);
+                    break;
+                case R.id.btn_dial_number:
+                    String phoneNumber = "082229385540";
+                    Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                    startActivity(dialPhoneIntent);
+                    break;
+
+            }
+    }
+}
